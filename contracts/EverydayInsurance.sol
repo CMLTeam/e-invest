3ece5dce7b93716ddf84853cbf632a2146bf8802
vgorin <vgrn.at@gmail.com>
2017-10-07 14:40:36 +0300

    Smart Contract API mockups

--- contracts/EverydayInsurance.sol
+++ contracts/EverydayInsurance.sol
@@ -7,6 +7,16 @@ contract EverydayInsurance {
 
 	}
 
+	// insurance fees and investments are accepted
+	// only before "offset" date (set-up period)
+	uint offset; // unix timestamp
+
+	// claims from insured are accepted
+	// only after "offset" has passed and
+	// only during "length" period of time (main aka claim period)
+	// investors' rewards are payed after (reward period)
+	uint length; // unix timestamp
+
 	address insurer;
 
 	mapping(address => uint) investors;
@@ -15,5 +25,67 @@ contract EverydayInsurance {
 	function EverydayInsurance() {
 	}
 
+	// client entrance (insured)
+	// sign of an insurance contract
+	// (buying an insurance product)
+	function insure() payable {
+		// perform validations
+		assert(now < offset); // main period didn't start yet
+
+		// call sender gracefully, an insured
+		address insured = msg.sender;
+
+		// TODO: implement logic
+	}
+
+	// client entrance (insured)
+	// submit a claim
+	function claim() {
+		// perform validations
+		assert(now >= offset && now < offset + length); // main period
+
+		// call sender gracefully, an insured
+		address insured = msg.sender;
+
+		// TODO: implement logic
+	}
+
+
+	// insurer entrance
+	// approve a claim
+	function approve() {
+		// perform validations
+		assert(now >= offset && now < offset + length); // main period
+
+		// TODO: implement logic
+	}
+
+	// insurer entrance
+	// decline a claim
+	function decline() {
+		// perform validations
+		assert(now >= offset && now < offset + length); // main period
+
+		// TODO: implement logic
+	}
+
+
+	// investor entrance
+	// make an investment
+	function invest() {
+		// perform validations
+		assert(now < offset); // main period didn't start yet
+
+		// TODO: implement logic
+	}
+
+	// investor entrance
+	// take an investment and profit back
+	function pick() {
+		// perform validations
+		assert(now >= offset + length); // main period ended
+
+		// TODO: implement logic
+	}
 
 }
